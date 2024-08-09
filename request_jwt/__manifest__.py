c35badb0914d82e4baf0a7b760a202450c6fb85f
Enieber Cunha <eniebercunha@gmail.com>
2024-08-09 16:08:59 -0300

    feat: add icon and description of app

--- request_jwt/__manifest__.py
+++ request_jwt/__manifest__.py
@@ -1,16 +1,18 @@
 # Copyright 2021 ACSONE SA/NV
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 {
-    "name": "Auth JWT & Custom Endpoints",
+    "name": "Request endpoints with JWT",
     "summary": """
-        Custom module for auth_jwt and custom endpoints.""",
+        Custom module for auth_jwt and custom endpoints.
+    """,
     "version": "16.0.1.1.1",
     "license": "LGPL-3",
-    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "category": "Applications",
+    "author": "PopSolutions <pop.coop>",
     "maintainers": ["sbidoul"],
-    "website": "https://github.com/OCA/server-auth",
+    "website": "https://github.com/popsolutions/odoo_api_server",
     "depends": ["auth_jwt"],
+    "images": ["static/description/icon.png"],
     "data": ["data/auth_jwt_validator.xml"],
     "demo": ["demo/auth_jwt_validator.xml"],
 }
