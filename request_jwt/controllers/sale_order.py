17fe20617a6853784a3ff46ff97a8259d1776033
Enieber Cunha <eniebercunha@gmail.com>
2024-09-27 17:17:59 +0000

    feat: add fields in sales order

--- request_jwt/controllers/sale_order.py
+++ request_jwt/controllers/sale_order.py
@@ -1,15 +1,15 @@
 """Controller for the sale order model."""
-
+import logging
 import json
 from odoo.http import Controller, Response, request, route
 
 from .utils import clean_html
 
+_logger = logging.getLogger(__name__)
 
 class JWTSaleOrderController(Controller):
     """Controller to handle sale order records.
     - [GET] /sale_order: get all sale order records.
-    - [GET] /sale_order/<int:order_id>: get a sale order record by id.
     - [POST] /sale_order: create a sale order record.
     """
 
@@ -36,6 +36,8 @@ class JWTSaleOrderController(Controller):
                     "partner_id": order.partner_id.id,
                     "amount_total": order.amount_total,
                     "state": order.state or "N/A",
+                    "pricelist_id": order.pricelist_id.id,
+                    "payment_term_id": order.payment_term_id.id,
                     "lines": [
                         {
                             "id": line.id,
@@ -50,49 +52,6 @@ class JWTSaleOrderController(Controller):
                 for order in sale_order
             ]
         )
-        return Response(json.dumps(data), content_type="application/json", status=200)
-
-    @route(
-        "/api/sale_order/<int:order_id>",
-        type="http",
-        auth="jwt_api",
-        csrf=False,
-        cors="*",
-        save_session=False,
-        methods=["GET", "OPTIONS"],
-    )
-    def get_sale_order_by_id(self, order_id):
-        """Get a sale order record by id.
-        - Return a JSON object with the sale order record.
-        - Return a JSON object with an error if the sale order record is not found.
-        """
-        data = {}
-        sale_order = (
-            request.env["sale.order"].with_user(request.env.uid).browse(order_id)
-        )
-        if sale_order:
-            data.update(
-                sale_order={
-                    "name": sale_order.name,
-                    "date": str(sale_order.date_order),
-                    "id": sale_order.id,
-                    "partner_id": sale_order.partner_id.id,
-                    "amount_total": sale_order.amount_total,
-                    "state": sale_order.state or "N/A",
-                    "lines": [
-                        {
-                            "id": line.id,
-                            "product_id": line.product_id.id,
-                            "product_name": line.product_id.name,
-                            "quantity": line.product_uom_qty,
-                            "price": line.price_unit,
-                        }
-                        for line in sale_order.order_line
-                    ],
-                }
-            )
-        else:
-            data.update({"error": "Sale order not found."})
         return Response(json.dumps(data), content_type="application/json", status=200)
 
     @route(
@@ -120,6 +79,8 @@ class JWTSaleOrderController(Controller):
                         "date_order": payload.get("date"),
                         "partner_id": payload.get("partner_id"),
                         "amount_total": payload.get("amount_total"),
+                        "pricelist_id": payload.get("pricelist_id"),
+                        "payment_term_id": payload.get("payment_term_id"),
                         "order_line": [
                             (
                                 0,
@@ -155,9 +116,11 @@ class JWTSaleOrderController(Controller):
                     ],
                 }
             )
+            return Response(json.dumps(data), content_type="application/json", status=200)
         except Exception as e:
             data.update({"error": str(e)})
-        return Response(json.dumps(data), content_type="application/json", status=200)
+            _logger.info("Error to save order: %s", str(e))
+            return Response(json.dumps(data), content_type="application/json", status=500)
 
     @route(
         "/api/sale_order/payment_terms",
